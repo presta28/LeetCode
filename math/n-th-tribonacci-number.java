669b129613ffc777cab8511e44aef732de6c7b47
presta <prestachoudhary81@gmail.com>
2026-06-19 22:43:06 +0530

    LC-n-th-tribonacci-number [Runtime: N/A; Memory: N/A]

--- math/n-th-tribonacci-number.java
+++ math/n-th-tribonacci-number.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int tribonacci(int n) {
+    int[] dp;
+    public int solve(int n){
+        if(n==1||n==2){
+            return 1;
+        }
         if(n==0){
             return 0;
         }
-        if(n==1){
-            return 1;
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        if(n==2){
-            return 1;
-        }
-        else{
-            n=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-        }
-    return n;   
+        dp[n]=solve(n-1)+solve(n-2)+solve(n-3);
+        return dp[n];
     }
+    public int tribonacci(int n){
+        dp=new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n);
+    }     
 }
