2cdb9630c07ac088ea232034eeae26328f92b26e
presta <prestachoudhary81@gmail.com>
2026-06-29 18:11:34 +0530

    LC-maximum-number-of-vowels-in-a-substring-of-given-length [Runtime: N/A; Memory: N/A]

---
+++ string/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        current_count=0
+        for i in range(k):
+            if s[i] in "aeiou":
+                current_count+=1
+        max_count=current_count
+        for i in range(k,len(s)):
+            current_count=0
+            s=s[i:i+k]
+            for ch in s:
+                if ch in"aeiou":
+                    current_count+=1
+                if current_count>max_count:
+                    max_count=current_count
+        return max_count     
