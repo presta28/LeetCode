bed4927898c290030b2597d2dc9962cc0cae56d5
presta <prestachoudhary81@gmail.com>
2026-07-04 14:00:53 +0530

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- two-pointers/move-zeroes.py
+++ two-pointers/move-zeroes.py
@@ -1,7 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         i=0
-        j=1
+        j=0
         while j<len(nums):
             if nums[j]!=0:
                 nums[i],nums[j]=nums[j],nums[i]
