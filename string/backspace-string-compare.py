df25744249d8b1d8cb43cd7475a3530e4e71d76d
presta <prestachoudhary81@gmail.com>
2026-07-07 15:32:54 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- string/backspace-string-compare.py
+++ string/backspace-string-compare.py
@@ -1,20 +1,62 @@
+# class Solution:
+#     def backspaceCompare(self, s: str, t: str) -> bool:
+#         stack1 = []
+#         stack2 = [] 
+#         for ch in s:
+#             if ch!='#':
+#                 stack1.append(ch)
+#             else:
+#                 if len(stack1) != 0:
+#                     stack1.pop()
+#         for ch in t:
+#             if ch!='#':
+#                 stack2.append(ch)
+#             else:
+#                 if len(stack2) != 0:
+#                     stack2.pop()
+#         if stack1==stack2:
+#             return True
+#         else:
+#             return False
+
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack1 = []
-        stack2 = [] 
-        for ch in s:
-            if ch!='#':
-                stack1.append(ch)
-            else:
-                if len(stack1) != 0:
-                    stack1.pop()
-        for ch in t:
-            if ch!='#':
-                stack2.append(ch)
-            else:
-                if len(stack2) != 0:
-                    stack2.pop()
-        if stack1==stack2:
-            return True
-        else:
-            return False
+        i = len(s) - 1
+        j = len(t) - 1
+
+        skip_s = 0
+        skip_t = 0
+
+        while i >= 0 or j >= 0:
+
+            while i >= 0:
+                if s[i] == '#':
+                    skip_s += 1
+                    i -= 1
+                elif skip_s > 0:
+                    skip_s -= 1
+                    i -= 1
+                else:
+                    break
+
+            while j >= 0:
+                if t[j] == '#':
+                    skip_t += 1
+                    j -= 1
+                elif skip_t > 0:
+                    skip_t -= 1
+                    j -= 1
+                else:
+                    break
+
+            if i >= 0 and j >= 0:
+                if s[i] != t[j]:
+                    return False
+
+            elif i >= 0 or j >= 0:
+                return False
+
+            i -= 1
+            j -= 1
+
+        return True
