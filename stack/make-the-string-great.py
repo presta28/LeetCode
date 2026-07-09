f94a88993f0b6a833d5cc621c0da07e6cd012a15
presta <prestachoudhary81@gmail.com>
2026-07-09 11:06:48 +0530

    LC-make-the-string-great [Runtime: N/A; Memory: N/A]

---
+++ stack/make-the-string-great.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = [s[0]]
+        for ch in s[1:]:
+            if len(stack)!=0:
+                if ch.lower()==stack[-1].lower() and ch!=stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(ch)
+        ans = ""
+        for ch in stack:
+            ans = ans+ch
+        return ans
