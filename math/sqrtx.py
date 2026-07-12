c6082a7e4f16f070e7030ca0fc0b356bf391a3ce
presta <prestachoudhary81@gmail.com>
2026-07-12 12:16:14 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.py
+++ math/sqrtx.py
@@ -1,10 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        left =0
-        while left<=x:
-            ans = left*left
-            if ans <= x:
-                sqrt = left
-            left+=1
-        return sqrt
-        
+        if x==0:
+            return 0
+        if x==1:
+            return 1
+        i=1
+        while i*i<x:
+            left = i
+            right = i*2
+            i=i*2
+        ans = 0
+        for i in range(left,right+1):
+            if i*i<=x:
+                ans = i
+        return ans
+
+            
