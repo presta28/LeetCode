306081e798a97bbb55a8fa392157c62e9dc489c7
presta <prestachoudhary81@gmail.com>
2026-07-25 10:52:48 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- two-pointers/linked-list-cycle.py
+++ two-pointers/linked-list-cycle.py
@@ -4,15 +4,28 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         sett=set()
+#         if head==None:
+#             return False
+#         current=head
+#         while current.next is not None:
+#             if current in sett:
+#                 return True
+#             sett.add(current)
+#             current =current.next
+#         return False
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        sett=set()
-        if head==None:
-            return False
-        current=head
-        while current.next is not None:
-            if current in sett:
+        slow = head
+        current = head
+        fast=head
+        while current is not None:
+            if fast.next==None or fast.next.next==None:
+                return False
+            if slow==fast:
                 return True
-            sett.add(current)
-            current =current.next
-        return False
+            slow=slow.next
+            fast = fast.next.next
