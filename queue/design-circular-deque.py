e09ef679489f491a26aa741fa551c5fe774a361c
presta <prestachoudhary81@gmail.com>
2026-08-02 10:58:29 +0530

    LC-design-circular-deque [Runtime: N/A; Memory: N/A]

---
+++ queue/design-circular-deque.py
@@ -0,0 +1,71 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.deque=[None]*k
+        self.capacity=k
+        self.front=0
+        self.rear=-1
+        self.size=0
+
+    def insertFront(self, value: int) -> bool:
+        if self.size==self.capacity:
+            return False
+        self.front=(self.front-1+self.capacity)%self.capacity
+        self.deque[self.front]=value
+        self.size+=1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.size==self.capacity:
+            return False
+        self.rear=(self.rear+1)%self.capacity
+        self.deque[self.rear]=value
+        self.size+=1
+        return True
+    def deleteFront(self) -> bool:
+        if self.size==1:
+            self.front=0
+            self.rear=-1
+            self.size=0
+            return 
+        self.front=(self.front+1)%self.capacity
+        self.size-=1
+        return True
+        
+    def deleteLast(self) -> bool:
+        if self.size==1:
+            self.front=0
+            self.rear=-1
+            self.size=0
+        self.rear=(self.front-1+self.capacity)%self.capacity
+        self.size-=1
+        return True
+        
+
+    def getFront(self) -> int:
+        return self.deque[self.front]
+
+    def getRear(self) -> int:
+        return self.deque[self.rear]
+        
+
+    def isEmpty(self) -> bool:
+        if self.size==0:
+            return True
+        return False
+    def isFull(self) -> bool:
+        if self.size==self.capacity:
+            return True
+        return False
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
