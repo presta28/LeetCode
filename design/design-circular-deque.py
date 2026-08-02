66c2c03d0f1d82691c35d82e444774ecdf5095fb
presta <prestachoudhary81@gmail.com>
2026-08-02 11:19:27 +0530

    LC-design-circular-deque [Runtime: N/A; Memory: N/A]

--- design/design-circular-deque.py
+++ design/design-circular-deque.py
@@ -8,11 +8,20 @@ class MyCircularDeque:
         self.size=0
 
     def insertFront(self, value: int) -> bool:
-        if self.size==self.capacity:
+        if self.size == self.capacity:
             return False
-        self.front=(self.front-1+self.capacity)%self.capacity
-        self.deque[self.front]=value
-        self.size+=1
+
+        if self.size == 0:
+            self.front = 0
+            self.rear = 0
+        else:
+            self.front = (
+                self.front - 1 + self.capacity
+            ) % self.capacity
+
+        self.deque[self.front] = value
+        self.size += 1
+
         return True
 
     def insertLast(self, value: int) -> bool:
@@ -23,13 +32,17 @@ class MyCircularDeque:
         self.size+=1
         return True
     def deleteFront(self) -> bool:
-        if self.size==1:
-            self.front=0
-            self.rear=-1
-            self.size=0
-            return True
-        self.front=(self.front+1)%self.capacity
-        self.size-=1
+
+        if self.size == 0:
+            return False
+
+        if self.size == 1:
+            self.front = 0
+            self.rear = -1
+        else:
+            self.front = (self.front + 1) % self.capacity
+
+        self.size -= 1
         return True
         
     def deleteLast(self) -> bool:
@@ -38,15 +51,19 @@ class MyCircularDeque:
             self.rear=-1
             self.size=0
             return True
-        self.rear=(self.front-1+self.capacity)%self.capacity
+        self.rear=(self.rear-1+self.capacity)%self.capacity
         self.size-=1
         return True
         
 
     def getFront(self) -> int:
+        if self.size == 0:
+            return -1
         return self.deque[self.front]
 
     def getRear(self) -> int:
+        if self.size == 0:
+            return -1
         return self.deque[self.rear]
         
 
