5bc7129aab1be442ab2abea5c1cd502a1ed3d492
presta <prestachoudhary81@gmail.com>
2026-08-08 10:26:05 +0530

    LC-binary-tree-preorder-traversal [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/binary-tree-preorder-traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        list1=[]
+        def preorder(node:optional[TreeNode]):
+            if node==None:
+                return
+            list1.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return list1
+        
