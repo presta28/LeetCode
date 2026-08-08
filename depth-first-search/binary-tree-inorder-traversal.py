07c3a3f0c55f30c6c386da73b98b79ef878a38ee
presta <prestachoudhary81@gmail.com>
2026-08-08 10:47:51 +0530

    LC-binary-tree-inorder-traversal [Runtime: 0 ms; Memory: 19.4 MB]

---
+++ depth-first-search/binary-tree-inorder-traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        list1=[]
+        def inorder(node:optional[TreeNode]):
+            if node==None:
+                return
+            inorder(node.left)
+            list1.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        return list1
+        
